a792abdd524ed8e8cb4ff9abfb6097de6c74f7be
Dhirendra Singh <dhiirendra8@gmail.com>
2018-08-27 17:40:07 +0530

    c

--- src/main/java/com/codejava/spring/utils/Constants.java
+++ src/main/java/com/codejava/spring/utils/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
 	//local
 	public static final String path = "C:/EnvManagement/SheetFolder/";
 	
+	
+	
 	public static final String driver = "org.apache.derby.jdbc.ClientDriver";
 	public static final String url = "jdbc:derby://localhost:1527/mydb;create=true";
 	public static final String username = null;
